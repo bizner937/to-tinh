bd54036ffa1a39996e1c419b6ce5843cc50d5926
bizner937 <113236926+bizner937@users.noreply.github.com>
2022-09-10 16:47:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const CONFIG = {
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'ỏ bấm thích mới vô dc đây v là thích anh rồi !!! có gì nói vs anh hem <3',
-    btnReply: 'em thích anh lắm :))) ',
-    reply: 'v là hết ùi bái bai ',
+    btnReply: 'v là hết ùi bái bai ',
+    reply: ' em thích anh lắm :)))😘😘 ',
     mess: 'anh biết mà 🥰 😘😘',
     messLink: 'https://www.facebook.com/messages/t/100060267054281/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
